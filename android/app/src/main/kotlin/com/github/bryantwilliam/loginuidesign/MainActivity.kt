13235ffb0c76bba688b3b8ad699dc3a7840cb00e
bryantwilliam <gogobebe2@gmail.com>
2023-06-01 04:32:28 +1200

    first commit

---
+++ android/app/src/main/kotlin/com/github/bryantwilliam/loginuidesign/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.github.bryantwilliam.loginuidesign
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/login_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  final Color _primaryColor = const Color.fromRGBO(125, 191, 211, 1);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: _primaryColor,
+    );
+  }
+}
